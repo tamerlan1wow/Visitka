e453ec5c4565b52b0a154cef523eac3f889f4702
Tamerlan <slatergt228@gmail.com>
2025-12-31 17:43:44 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      > 
+        <main className="`min-h-screen w-full ${jetBrainsMono.variable} antialiased`">{children}</main>
       </body>
     </html>
   );
